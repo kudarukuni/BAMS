7b2c121910597e5d1b14da52ab1989fc11809b01
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2022-11-22 21:28:24 +0200

    Update FetchCandyMachine.tsx

--- src/components/FetchCandyMachine.tsx
+++ src/components/FetchCandyMachine.tsx
@@ -25,6 +25,7 @@ export const FetchCandyMachine: FC = () => {
       const candyMachine = await metaplex
         .candyMachines()
         .findByAddress({ address: new PublicKey(candyMachineAddress) })
+        .run()
 
       setCandyMachineData(candyMachine)
     } catch (e) {
