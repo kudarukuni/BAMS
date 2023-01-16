e28057b2a1a404ef98c6cc4e3b7f1c933eb25aef
Kuda Rukuni <52784179+kudarukuni@users.noreply.github.com>
2023-01-16 22:04:28 +0200

    Update FetchCandyMachine.tsx

--- src/components/FetchCandyMachine.tsx
+++ src/components/FetchCandyMachine.tsx
@@ -6,7 +6,7 @@ import styles from "../styles/custom.module.css"
 
 export const FetchCandyMachine: FC = () => {
   const [candyMachineAddress, setCandyMachineAddress] = useState(
-    "o5JXCZ5du6E1Abg1beS26ubG4Qv2aodNRynkn9UqrNa"
+    "ABDrLxrL2mjyokRVqKbP386MJEKZtKPBfZaEuC5RaD1f"
   )
   const [candyMachineData, setCandyMachineData] = useState(null)
   const [pageItems, setPageItems] = useState(null)
